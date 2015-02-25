ff46408c4da8b078fc8d227a63bc6711175747e9
Johnathan Howell <me@johnathanhowell.com>
2015-02-24 23:57:47 -0500

    added sounds flag

--- gumblebot.go
+++ gumblebot.go
@@ -8,21 +8,27 @@ import (
 	"github.com/layeh/gumble/gumble_ffmpeg"
 	"github.com/layeh/gumble/gumble"
 	"github.com/layeh/gumble/gumbleutil"
+	"flag"
 )
 
 
 func main() {
+	var sounds_dir = flag.String("sounds", "sounds", "directory where soundboard files are located")
+	var stream *gumble_ffmpeg.Stream
 	soundboard := make(map[string]string)
 
 	// Populate soundboard with files in $pwd/sounds/
-	files, _ := ioutil.ReadDir("./sounds")
+	files, err := ioutil.ReadDir(*sounds_dir)
+	if err != nil  {
+		fmt.Println(err)
+		return
+	}
 	for _, f := range files {
 		key := f.Name()
-		value, _ := filepath.Abs(filepath.Join("sounds/", key))
+		value, _ := filepath.Abs(filepath.Join(*sounds_dir, key))
 		soundboard[key] = value
 	}
 
-	var stream *gumble_ffmpeg.Stream
 
 	gumbleutil.Main(func(client *gumble.Client) {
 		stream, _ = gumble_ffmpeg.New(client)
