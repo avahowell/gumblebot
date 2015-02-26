0f37bdf729246f7f5b9f825afb7971839c884e19
Johnathan Howell <me@johnathanhowell.com>
2015-02-26 03:05:47 -0500

    added and implemented Soundboard type

--- gumblebot.go
+++ gumblebot.go
@@ -36,8 +36,76 @@ type SoundboardUser struct {
 	SoundboardVolume float64
 	WelcomeSound string
 }
-
-var soundboard_users map[string]SoundboardUser
+type Soundboard struct {
+	Users map[string]SoundboardUser
+	sounds map[string]string
+}
+func (s *Soundboard) LoadSounds(path string) {
+	s.sounds = make(map[string]string)
+	files,err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, f := range files {
+		key := f.Name()
+		if filepath.Ext(key) == ".mp3" || filepath.Ext(key) == ".ogg" || filepath.Ext(key) == ".wav" {
+			value, _ := filepath.Abs(filepath.Join(path, key))
+			s.sounds[key] = value
+		}
+	}
+}
+func (s *Soundboard) LoadUsers(datapath string) {
+	iobuffer, err := ioutil.ReadFile(datapath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	buffer := bytes.NewBuffer(iobuffer)
+	dec := gob.NewDecoder(buffer)
+	err = dec.Decode(&s.Users)
+	if err != nil {
+		panic(err)
+	}
+}
+func (s *Soundboard) SaveUsers(datapath string) {
+	var buffer bytes.Buffer
+	enc := gob.NewEncoder(&buffer)
+	err := enc.Encode(s.Users)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(datapath, buffer.Bytes(), 0755)
+	if err != nil {
+		panic(err)
+	}
+}
+func (s *Soundboard) UpdateUsers(client *gumble.Client) {
+	for _, user := range client.Users {
+		if _, ok := s.Users[user.Name]; ok {
+			continue
+		}
+		s.Users[user.Name] = SoundboardUser{false, 0.5, ""}
+	}
+}
+func (s *Soundboard) Play(client *gumble.Client, stream *gumble_ffmpeg.Stream, sound string)  {
+	for key, value := range s.sounds {
+		if strings.Index(key, sound) == 0 {
+			vtarget := &gumble.VoiceTarget{}
+			vtarget.ID = 1
+			for username, sb := range s.Users {
+				userstruct := client.Users.Find(username)
+				if userstruct != nil && sb.SoundboardEnabled == true {
+					vtarget.AddUser(userstruct)
+				}
+			}
+			stream.Stop()
+			client.Send(vtarget)
+			client.VoiceTarget = vtarget
+			stream.Play(value)
+		}
+	}
+}
 
 func send_usage(client *gumble.Client, soundboard map[string]string) {
 	var buffer bytes.Buffer
@@ -59,70 +127,17 @@ func send_usage(client *gumble.Client, soundboard map[string]string) {
 	}
 	client.Send(&message)
 }
-func decode_users() {
-	iobuffer, err := ioutil.ReadFile(datafile)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	buffer := bytes.NewBuffer(iobuffer)
-	dec := gob.NewDecoder(buffer)
-
-	err = dec.Decode(&soundboard_users)
-	if err != nil {
-		panic(err)
-	}
-}
-func encode_users() {
-	var buffer bytes.Buffer
-	enc := gob.NewEncoder(&buffer)
-	err := enc.Encode(soundboard_users)
-	if err != nil {
-		panic(err)
-	}
-	err = ioutil.WriteFile(datafile, buffer.Bytes(), 0755)
-	if err != nil {
-		panic(err)
-	}
-}
-func update_users(client *gumble.Client) {
-	for _, user := range client.Users {
-		if _, ok := soundboard_users[user.Name]; ok {
-			continue
-		}
-		soundboard_users[user.Name] = SoundboardUser{false, 0.5, ""}
-		encode_users()
-	}
-}
-func init() {
-	soundboard_users = make(map[string]SoundboardUser)
-
-	decode_users()
-}
-
 
 func main() {
 	var sounds_dir = flag.String("sounds", "sounds", "directory where soundboard files are located")
 	var volume = flag.Float64("volume", 0.25, "soundboard volume from 0 to 1")
 	var stream *gumble_ffmpeg.Stream
-	soundboard := make(map[string]string)
 
-	// Populate soundboard with files in $pwd/sounds/
-	files, err := ioutil.ReadDir(*sounds_dir)
-	if err != nil  {
-		fmt.Println(err)
-		return
-	}
-	for _, f := range files {
-		key := f.Name()
-		if filepath.Ext(key) == ".mp3" || filepath.Ext(key) == ".ogg" || filepath.Ext(key) == ".wav" {
-			value, _ := filepath.Abs(filepath.Join(*sounds_dir, key))
-			soundboard[key] = value
-		}
-	}
-
-
+	var soundboard Soundboard
 	var gumbleclient *gumble.Client
+
+	soundboard.LoadUsers(datafile)
+	soundboard.LoadSounds(*sounds_dir)
 
 	gumbleutil.Main(func(client *gumble.Client) {
 		stream, _ = gumble_ffmpeg.New(client)
@@ -132,7 +147,8 @@ func main() {
 	}, gumbleutil.Listener{
 		Connect: func(e *gumble.ConnectEvent) {
 			fmt.Printf("Connected!\n")
-			update_users(gumbleclient)
+			soundboard.UpdateUsers(gumbleclient)
+			soundboard.SaveUsers(datafile)
 		},
 		TextMessage: func(e *gumble.TextMessageEvent) {
 			if e.Sender == nil {
@@ -142,39 +158,25 @@ func main() {
 				stream.Stop()
 			}
 			if e.Message == "help" {
-				go send_usage(gumbleclient, soundboard)
+				go send_usage(gumbleclient, soundboard.sounds)
 			}
 			if e.Message == "sboff" {
-				u := soundboard_users[e.Sender.Name]
+				u := soundboard.Users[e.Sender.Name]
 				u.SoundboardEnabled = false
-				soundboard_users[e.Sender.Name] = u
-				encode_users()
+				soundboard.Users[e.Sender.Name] = u
+				soundboard.SaveUsers(datafile)
 			}
 			if e.Message == "sbon" {
-				u := soundboard_users[e.Sender.Name]
+				u := soundboard.Users[e.Sender.Name]
 				u.SoundboardEnabled = true
-				soundboard_users[e.Sender.Name] = u
-				encode_users()
+				soundboard.Users[e.Sender.Name] = u
+				soundboard.SaveUsers(datafile)
 			}
-			for key, value := range soundboard {
-				if strings.Index(key, e.Message) == 0 {
-					vtarget := &gumble.VoiceTarget{}
-					vtarget.ID = 1
-					for username, sb := range soundboard_users {
-						userstruct := gumbleclient.Users.Find(username)
-						if userstruct != nil && sb.SoundboardEnabled == true {
-							vtarget.AddUser(userstruct)
-						}
-					}
-					stream.Stop()
-					gumbleclient.Send(vtarget)
-					gumbleclient.VoiceTarget = vtarget
-					stream.Play(value)
-				}
-			}
+			soundboard.Play(gumbleclient, stream, e.Message)
 		},
 		UserChange: func(e *gumble.UserChangeEvent) {
-			update_users(gumbleclient)
+			soundboard.UpdateUsers(gumbleclient)
+			soundboard.SaveUsers(datafile)
 		},
 	})
 
